0088e40fd9096179b201ab3cd89572e3a021cfdd
sce06147 <sce06147@scg.skku.ac.kr>
2023-03-30 11:48:45 +0900

    feat(infra): aws-nuke 기반 초기화 배치파일 추가

---
+++ AWS초기화.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Author : Hyunsu Ko
+echo Updated : 2022.03.30
+echo 본 프로그램은 https://github.com/rebuy-de/aws-nuke.git를 바탕으로 제작되었습니다.
+echo.
+echo.
+echo.
+
+aws-nuke.exe -c config.yml --no-dry-run
+pause
